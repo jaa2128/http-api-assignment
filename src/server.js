0f8221e731fcf54323303fb5c1d82407034594dc
Julian Alvia (RIT Student) <jaa2128@rit.edu>
2026-02-04 12:48:05 -0500

    Have responses for client.html and style.css implemented

---
+++ src/server.js
@@ -0,0 +1,35 @@
+// http and handlers
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+// Port
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCss,
+    index: htmlHandler.getIndex
+};
+
+const onRequest = (request, response) => {
+    const protocol = request.connection.encrypted ? 'https' : 'http';
+    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+
+    if(request.headers.accept){
+        request.acceptedTypes = request.headers.accept.split(',');
+    }
+
+    const handler = urlStruct[parsedUrl.pathname];
+    if(handler){
+        handler(request, response);
+    } 
+    else{
+        urlStruct.index(request, response);
+    }
+};
+
+// Create Server 
+http.createServer(onRequest).listen(port, () => {
+    console.log(`Listening on 127.0.0.1:${port}`);
+});
